25ef54cfa3885957eefbe8c2f7ea4e588ffd157b
Henrik Gustavii <Henrik@Henriks-MacBook-Pro.local>
2017-08-24 08:19:23 +0200

    Initial version. So far just worked out how to separate the different blog titles and their respective contents. Storing locally in CoreData next...

---
+++ BlogReader/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  BlogReader
+//
+//  Created by Henrik Gustavii on 13/08/2017.
+//  Copyright © 2017 aecasorg. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
